a38db10706d2b17bce791d6cb7a9c439ed7f67e9
snail007 <arraykeys@gmail.com>
2023-09-01 19:33:52 +0800

    fix sword make all

--- sword/resource/assets_path.go
+++ sword/resource/assets_path.go
@@ -1,13 +1,13 @@
 package resource
 
 var AssetPaths = map[string]string{
-	"all.min.css":      "/dist/css/all.min.79250b7051.css",
-	"all.min.js":       "/dist/js/all.min.8425540791.js",
-	"all_2.min.js":     "/dist/js/all_2.min.38a2a946b0.js",
-	"datatable.min.js": "/dist/js/datatable.min.581cdc109b.js",
-	"form.min.js":      "/dist/js/form.min.f8678914e9.js",
+	"all.min.css":      "/dist/css/all.min.782fc50d44.css",
+	"all.min.js":       "/dist/js/all.min.7a3d41e2eb.js",
+	"all_2.min.js":     "/dist/js/all_2.min.9d3409635b.js",
+	"datatable.min.js": "/dist/js/datatable.min.759e79556d.js",
+	"form.min.js":      "/dist/js/form.min.d73c3493e6.js",
 	"html5shiv.min.js": "/dist/js/html5shiv.min.js",
 	"respond.min.js":   "/dist/js/respond.min.js",
-	"tree.min.js":      "/dist/js/tree.min.e1faf8b7de.js",
-	"treeview.min.js":  "/dist/js/treeview.min.7780d3bb0f.js",
+	"tree.min.js":      "/dist/js/tree.min.7e1c833f67.js",
+	"treeview.min.js":  "/dist/js/treeview.min.254dbb5c8b.js",
 }
